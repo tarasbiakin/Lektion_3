cbb2bbce961c9e8d4b844e5e2bbd7179b045bc46
tarasbiakin <115660593+tarasbiakin@users.noreply.github.com>
2022-11-21 11:57:33 +0300

    цикл в цикле

--- arraus/Program.cs
+++ arraus/Program.cs
@@ -1,1 +1,37 @@
-﻿
+﻿// Сортировка массива методом мин-мах
+
+int[] arr = { 1, 3, 4, 5, 5, 6, 1, 8, 9 };
+
+//метод для вывода на печать
+void PrintArrau(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]}");
+
+    }
+    Console.WriteLine();
+}
+
+//Метод который сортирует массив
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[i] < array[minPosition]) minPosition = j;
+        }
+        //меняем мнимальную позицию и текущую местами
+        int teporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = teporary;
+    }
+}
+PrintArrau(arr);
+SelectionSort(arr);
+
+PrintArrau(arr);
